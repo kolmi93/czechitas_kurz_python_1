d586d9f8e60735fdd259e72cb1f045c4ee4b9ff1
Michaela Kolarova <kolarova.mi@gmail.com>
2023-03-29 11:45:48 +0200

    Oprava. Omylem jsem nahrála oba soubory najednou.

--- 1_ukol_nepovinny_Kolarova.py
+++ 1_ukol_nepovinny_Kolarova.py
@@ -4,9 +4,18 @@ print(name_surname.upper())
 
 print(name_surname.lower())
 
-jmena = name_surname.split(" ")
-jmena.upper([0][0])
-jmena.lower([0][1:])
-jmena.upper([1][0])
-jmena.lower([1][1:])
-print(jmena.upper([0][0]) + jmena.lower([0][1:]) +" " + jmena.upper([1][0]) + jmena.lower([1][1:]))
+list = name_surname.split(" ")
+name = str(list[0])
+surname = str(list [-1])
+name_1 = name[0]
+name_2 = name[1:]
+surname_1 = surname[0]
+surname_2 = surname[1:]
+print(str(name_1.upper() + name_2.lower() + " " + surname_1.upper() + surname_2.lower()))
+
+print(name[0] + "." + surname[0] + ".")
+
+if len(name)>=5:
+    print(name[0] + "." + surname)
+else:
+    print(name+surname)
