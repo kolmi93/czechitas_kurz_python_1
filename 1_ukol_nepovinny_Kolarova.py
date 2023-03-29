9e0606827655309cdda5667e0ec4d512495504ea
Michaela Kolarova <kolarova.mi@gmail.com>
2023-03-29 11:59:04 +0200

    oprava chyby

--- 1_ukol_nepovinny_Kolarova.py
+++ 1_ukol_nepovinny_Kolarova.py
@@ -13,7 +13,7 @@ surname_1 = surname[0]
 surname_2 = surname[1:]
 print(str(name_1.upper() + name_2.lower() + " " + surname_1.upper() + surname_2.lower()))
 
-print(name_1.upper () + "." + surname[0] + ".")
+print(name_1.upper () + "." + surname_1.upper() + ".")
 
 if len(name)>=5:
     name_3 = str(name[0])
