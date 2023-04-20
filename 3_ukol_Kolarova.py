56d6712faa6636a90889d452d3560255b812351d
Michaela Kolarova <kolarova.mi@gmail.com>
2023-04-20 20:25:28 +0200

    final

--- 3_ukol_Kolarova.py
+++ 3_ukol_Kolarova.py
@@ -1,17 +1,19 @@
 import json
-with open('body.json', encoding='utf-8') as file:
-    text = file.read()
 
+with open('body.json', encoding = 'utf-8') as file:
+    text = json.load(file)
 print(text)
+
 result = {}
 for key in text:
     score = text[key]
     if score >= 60:
-       result[key] = "Pass"
+        result[key] = "Pass"
     else:
-       result[key] = "Fail"
+        result[key] = "Fail"
+
 text.update(result)
+print(text)
 
-with open('body.json', mode='w', encoding='utf-8') as file:
-   json.dump(result, file)
-
+with open('prospech.json', mode='w', encoding='utf=8') as file:
+    json.dump(text, file, ensure_ascii=False)
