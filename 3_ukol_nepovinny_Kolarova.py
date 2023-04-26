5dfe19358ca424760b432e54c0bdb1c93540fde8
Michaela Kolarova <kolarova.mi@gmail.com>
2023-04-26 15:23:38 +0200

    final

--- 3_ukol_nepovinny_Kolarova.py
+++ 3_ukol_nepovinny_Kolarova.py
@@ -1,17 +1,33 @@
 import json
 
 with open('body.json', encoding = 'utf-8') as file:
-    students_list = json.load(file)
+    students_list = json.load(file) 
 
 with open('bonusy.json', encoding = 'utf-8') as file_2:
     bonuses = json.load(file_2)
 
-def merge_dict(students_list, bonuses):
-    new_dict = {**students_list, **bonuses}
-    for key, value in new_dict.items():
-        if key in students_list and key in bonuses:
-            new_dict[key] = [value, (students_list[key] + bonuses[key])]
-    return new_dict
-
-new_dict = merge_dict(students_list, bonuses)
+new_dict = {}
+for key in students_list:
+    new_dict[key] = students_list[key] + bonuses.get(key, 0)
+for key in bonuses:
+    if key not in new_dict:
+        new_dict[key] = bonuses[key]
 print(new_dict)
+
+marks = {}
+for key in new_dict:
+    score = new_dict[key]
+    if score >= 90:
+        marks[key] = 1
+    elif score >= 70 and score <= 89:
+        marks[key] = 2
+    elif score >= 50 and score <= 69:
+        marks[key] = 3
+    elif score >= 30 and score <= 49:
+        marks[key] = 4
+    else:
+        marks[key] = 5
+print(marks)
+
+with open('znamky.json', mode='w', encoding='utf=8') as file:
+    json.dump(marks, file, ensure_ascii=False)
