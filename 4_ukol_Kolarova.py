3b436770025981f1d077be5fad3027835dde153e
Michaela Kolarova <kolarova.mi@gmail.com>
2023-04-26 20:05:43 +0200

    oprava

--- 4_ukol_Kolarova.py
+++ 4_ukol_Kolarova.py
@@ -1,31 +1,28 @@
+import math
+
 phone_number = input("Na jaké telefonní číslo chcete zpávu odeslat?\n")
 
 phone_standart = phone_number.replace(" ", "")
 
 def phone_verification(phone_standart):
     if len(phone_standart) == 9:
-        print("true")
+        return True
     elif len(phone_standart) == 13 and phone_standart[0:4] == ("+420"):
-        print("true")
+        return True
     else:
-        print("false")
-
+        return False
+    
 phone_verification(phone_standart)
 
-message = input("Jakou zprávu chcete poslat?\n")
+if phone_verification(phone_standart) == True:
+    message = input("Jakou zprávu chcete poslat?\n")
+else:
+    print("Napsali jste špatný formát telefonního čísla. Prosím, zkuste to znovu.")
+    exit()
 
 def message_price(message):
-    if len(message) == 0:
-        price = 0
-        print(f"Cena Vaší zprávy je: {price} Kč.")
-    elif len(message) >= 1 and len(message) <= 180:
-        price = 3
-        print(f"Cena Vaší zprávy je: {price} Kč.")
-    elif (len(message)/180) == 0:
-        price = len(message)//180
-        print(f"Cena Vaší zprávy je: {price} Kč.")
-    else:
-        price = (len(message)//180 + 1)
-        print(f"Cena Vaší zprávy je: {price} Kč.")
+    lenght = math.ceil((len(message)/180))
+    price = (lenght*3)
+    print(f"Cena Vaší zprávy je: {price} Kč.")
 
 message_price(message)
