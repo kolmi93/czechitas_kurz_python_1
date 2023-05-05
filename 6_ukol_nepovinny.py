f0caf1912c7595695a13cec2a0acfb75f9f2584e
Michaela Kolarova <kolarova.mi@gmail.com>
2023-05-05 13:29:38 +0200

    final

--- 6_ukol_nepovinny.py
+++ 6_ukol_nepovinny.py
@@ -5,9 +5,9 @@ regular_string_1 = re.compile(r"[A-Za-z]{6,10}")
 print(regular_string_1.fullmatch(user_name))
 
 password = input("Zadejte heslo:\n")
-regular_string_2 = re.compile(r"\w[- . + =]{12,30}")
+regular_string_2 = re.compile(r"(\w[\- \. + =]{12,30})")
 print(regular_string_2.fullmatch(password))
 
 email = input("Zadejte Váš e-mail:\n")
-regular_string_3 = re.compile(r"\"?[A-Za-z0-9]*\-?\.?\+*[A-Za-z0-9_]+\"?@[A-Za-z0-9]+\-?[A-Za-z0-9]*[.][A-Za-z0-9]*[[com,net,museum,name,jp,co,cz]{1}\.?[net,museum,name,com,jp,co,cz]?")
+regular_string_3 = re.compile(r"\"?[A-Za-z0-9_]+\+*\-?\.?[A-Za-z0-9_]+\"?@[A-Za-z0-9]+\-?[A-Za-z0-9]*[.][A-Za-z0-9]*[[com,net,museum,name,jp,co,cz]{1}\.?[net,museum,name,com,jp,co,cz]?")
 print(regular_string_3.fullmatch(email))
