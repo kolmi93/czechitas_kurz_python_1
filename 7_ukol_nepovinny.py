e96f040f8c55716e2ef147cf55b0992f6dda7dc6
Michaela Kolarova <kolarova.mi@gmail.com>
2023-05-11 14:08:51 +0200

    final

--- 7_ukol_nepovinny.py
+++ 7_ukol_nepovinny.py
@@ -18,15 +18,13 @@ class Car():
 
     def return_car(self,return_km, days):
         self.avability = True
-        return_km = input("Uveďte stav tachometru při vrácení?\n")
-        if int(self.km) < int(return_km):
-            return_km == self.km
-        days = input("Kolik dnů jste měl/a vozidlo půjčeno?\n")
-        if int(days) < 7:
+        self.km = return_km
+        if days < 7:
             price=days*400
+            print(f"Za půjčení auta zaplatíte {price},- Kč.")
         else:
             price=days*300
-        return f"Za půjčení auta zaplatíte {price},- Kč."
+            print(f"Za půjčení auta zaplatíte {price},- Kč.")
 
 peugeot_403_cabrio = Car("4A2 3020","Peugeot 403 Cabrio", 47534)
 skoda_octavia = Car("1P3 4747","Škoda Octavia", 41253)
@@ -38,16 +36,20 @@ if wish == "Peugeot":
     print(peugeot_403_cabrio)
     peugeot_403_cabrio.get_car()
     print(peugeot_403_cabrio)
-    peugeot_403_cabrio.return_car(return_km, days)
+    q_return_km = int(input("Uveďte stav tachometru při vrácení?\n"))
+    q_days = int(input("Kolik dnů jste měl/a vozidlo půjčeno?\n"))
+    peugeot_403_cabrio.return_car(q_return_km, q_days)
     peugeot_403_cabrio.get_info()
-    print(peugeot_403_cabrio)
+    print(peugeot_403_cabrio.avability)
 elif wish == "Škoda":
     skoda_octavia.get_info()
     print(skoda_octavia)
     skoda_octavia.get_car()
     print(skoda_octavia)
-    skoda_octavia.return_car()
+    q2_return_km = input("Uveďte stav tachometru při vrácení?\n")
+    q2_days = input("Kolik dnů jste měl/a vozidlo půjčeno?\n")
+    skoda_octavia.return_car(q2_return_km, q2_days)
     skoda_octavia.get_info()
-    print(skoda_octavia)
+    print(skoda_octavia.avability)
 else:
     print("Vámi požadované vozidlo nemáme.")
